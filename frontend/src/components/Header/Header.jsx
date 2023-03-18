3bdf504fc43b75c7fda0f6dbaddd945dd995eb12
Thu <nightcore1506@gmail.com>
2023-03-18 10:30:29 +0700

    tourCard

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, {useRef,useEffect} from 'react';
 import {Container,Row,Button} from 'reactstrap'
 import {NavLink, Link} from 'react-router-dom'
-import logo from '../../assets/images/ .png'
+import logo from '../../assets/images/logo4.png'
 import './header.css'
 const Nav__link=[
   {
